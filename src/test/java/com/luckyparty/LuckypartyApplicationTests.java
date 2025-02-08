1d546a39f22219b701a5f1aef947d41b7f08b84e
unname <lwer210@naver.com>
2025-02-09 03:45:58 +0900

    Initial commit

---
+++ src/test/java/com/luckyparty/LuckypartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckyparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckypartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
