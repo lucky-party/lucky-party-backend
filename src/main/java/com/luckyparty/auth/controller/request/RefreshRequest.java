232c9fa06c30ccd00e20d3f6141b72f39f33e65e
unname <lwer210@naver.com>
2025-02-10 20:48:47 +0900

    feature: 토큰 재발급 요청 객체

---
+++ src/main/java/com/luckyparty/auth/controller/request/RefreshRequest.java
@@ -0,0 +1,13 @@
+package com.luckyparty.auth.controller.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshRequest {
+
+    private String refreshToken;
+}
