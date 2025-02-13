090382f63e43b52e81224ec22c2c159dba130014
unname <lwer210@naver.com>
2025-02-13 22:22:21 +0900

    feature: 이미 가입된 닉네임일 경우 예외 발생

---
+++ src/main/java/com/luckyparty/common/exception/AlreadyRegisteredNicknameException.java
@@ -0,0 +1,18 @@
+package com.luckyparty.common.exception;
+
+public class AlreadyRegisteredNicknameException extends RuntimeException {
+
+    private static final String DEFAULT_MESSAGE = "이미 가입된 닉네임입니다.";
+
+    public AlreadyRegisteredNicknameException() {
+        super(DEFAULT_MESSAGE);
+    }
+
+    public AlreadyRegisteredNicknameException(String message) {
+        super(message);
+    }
+
+    public AlreadyRegisteredNicknameException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
