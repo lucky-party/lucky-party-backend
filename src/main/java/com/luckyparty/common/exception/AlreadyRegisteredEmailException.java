8a574f4b97dd1c6833eeb014ae9ac8f34841f37f
unname <lwer210@naver.com>
2025-02-13 22:22:01 +0900

    feature: 이미 가입된 이메일일 경우 발생시킬 예외 추가

---
+++ src/main/java/com/luckyparty/common/exception/AlreadyRegisteredEmailException.java
@@ -0,0 +1,18 @@
+package com.luckyparty.common.exception;
+
+public class AlreadyRegisteredEmailException extends RuntimeException {
+
+    private static final String DEFAULT_MESSAGE = "이미 가입된 이메일입니다.";
+
+    public AlreadyRegisteredEmailException() {
+        super(DEFAULT_MESSAGE);
+    }
+
+    public AlreadyRegisteredEmailException(String message) {
+        super(message);
+    }
+
+    public AlreadyRegisteredEmailException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
